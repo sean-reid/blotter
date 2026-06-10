90a185222ca36bae4e42b1788abc8692e0d8ae2e
Sean Reid <seanreid.mail@gmail.com>
2026-06-10 12:38:40 -0700

    Compress pg_dump and clean stale partial dumps

--- infra/postgres/pg_dump.sh
+++ infra/postgres/pg_dump.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DUMP_DIR="/workspace/postgres-backup"
 mkdir -p "$DUMP_DIR"
-su postgres -c "pg_dump -Fc blotter" > "$DUMP_DIR/blotter.dump.new" \
+rm -f "$DUMP_DIR/blotter.dump.new"
+su postgres -c "pg_dump -Fc -Z 9 blotter" > "$DUMP_DIR/blotter.dump.new" \
   && mv "$DUMP_DIR/blotter.dump.new" "$DUMP_DIR/blotter.dump"
